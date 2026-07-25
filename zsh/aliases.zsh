d0fea1e1d6cbe8d86b7db5ace7a68a3ca458c189
isaiah-harville <harvillerisaiah@gmail.com>
2026-07-25 09:38:52 -0500

    chore: automated commit 2026-07-25 09:38:52

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,8 @@
 alias cpf='copyfile'
 alias gcm='git commit -m'
 alias no-sleep='caffeinate -dimsu'
+alias k='kubectl'
+alias tf='terraform'
 
 auto-commit() {
   git add . && git commit -m "chore: automated commit $(date '+%Y-%m-%d %H:%M:%S')" && git push
